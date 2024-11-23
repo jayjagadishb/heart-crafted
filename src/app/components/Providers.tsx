de3f1f778c7fb219c79272dcb7606fa2a7357285
Jagadish <jayjagadishb@gmail.com>
2024-11-24 03:00:38 +0530

    1st phase

---
+++ src/app/components/Providers.tsx
@@ -0,0 +1,14 @@
+import  React, {Children, ReactNode} from "react";
+import {NextUIProvider} from "@nextui-org/react";
+
+export default function Providers ({
+    children,
+  }:{ 
+    children: ReactNode;
+  }) {
+    return (
+        <NextUIProvider>
+       {children}
+      </NextUIProvider>
+    );
+  }
---
+++ src/app/members/page.tsx
@@ -0,0 +1,17 @@
+import { Link } from '@nextui-org/react'
+import React from 'react'
+
+const page = () => {
+  return (
+   <>
+   <div>
+    <h3>
+        Members page!
+    </h3>
+    <Link href='/'>Home</Link>
+   </div>
+   </>
+  )
+}
+
+export default page
